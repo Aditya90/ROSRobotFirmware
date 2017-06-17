a222b37f17ababdac028a202dccbf0a990dad055
Aditya Sreekumar <aditya.sreekumar@gmail.com>
2017-06-17 11:43:44 -0400

    Add a node for simple gpio control

---
+++ src/robot_motion_control/scripts/node_gpio_output.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+def motion_topic_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "Moving %s", data.data)
+
+def motion_topic_listener():
+    rospy.init_node('GpioOutput', anonymous=True)
+
+    rospy.Subscriber("MOTION_TOPIC", String, motion_topic_callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    motion_topic_listener()
