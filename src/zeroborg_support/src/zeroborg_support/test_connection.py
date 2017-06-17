9838627f5c957ea2eab2ce43d7909d162d17f890
Aditya Sreekumar <ubuntu@arm.localdomain>
2017-06-17 16:32:10 +0000

    script to check connection with zeroborg

---
+++ src/zeroborg_support/src/zeroborg_support/test_connection.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from zeroborg import ScanForZeroBorg
+from zeroborg import ZeroBorg
+
+#ZB=ZeroBorg()
+#ZB.i2cAddress=0x48
+#ZB.busNumber=0x02
+#ZB.Init()
+
+ScanForZeroBorg()
