70666e0eecf21495175aec80923ea5137284b529
Aditya Sreekumar <aditya.sreekumar@gmail.com>
2018-04-07 17:59:35 -0400

    feat(camera-support): Create support for the cv_camera image

--- src/camera_support/scripts/node_capture_camera.py
+++ src/camera_support/scripts/node_capture_camera.py
@@ -3,26 +3,45 @@
 '''
 import rospy
 from std_msgs.msg import String
+from sensor_msgs.msg import Image, CameraInfo
 
-def motion_topic_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "Received %s", data.data)
-    robotController.move_robot(data.data)
+import cv3
+import cv3.cv as cv
+import numpy as np
 
 
-def motion_topic_listener():
-    '''
-    Publisher for the CAMERA_TOPIC which sends the inputs from the camera
-    '''
-    rospy.init_node('CameraInput', anonymous=True)
+class ImageCapture:
 
-    rospy.subscriber("CAMERA_TOPIC", String, queue_size=2)
+    def __init__(self):
+        self.node_name = 'CameraInput'
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+        rospy.init_node(self.node_name, anonymous=True)
+
+        # What we do during shutdown
+        # rospy.on_shutdown(self.cleanup)
+
+        # Create the OpenCV display window for the RGB image
+        self.cv_window_name = self.node_name
+        cv.NamedWindow(self.cv_window_name, cv.CV_WINDOW_NORMAL)
+        cv.MoveWindow(self.cv_window_name, 25, 75)
+
+        rospy.subscriber("/cv_camera/image_raw", Image, image_capture_callback)
+
+    def image_capture_callback(self, opencv_image):
+        # Convert the image to a Numpy array since most cv2 functions
+        # require Numpy arrays.
+        frame = np.array(opencv_image, dtype=np.uint8)
+
+        # Process the frame using the process_image() function
+        #display_image = self.process_image(frame)
+        display_image = frame
+        
+        # Display the image.
+        cv3.imshow(self.node_name, display_image)
 
 if __name__ == '__main__':
 
     # Init the raspirobot with the right voltages
-    robotController = RaspiRobot(BATTERY_VOLTAGE, MOTOR_VOLTAGE)
+    ImageCapture()
 
-    motion_topic_listener()
+    rospy.spin()
