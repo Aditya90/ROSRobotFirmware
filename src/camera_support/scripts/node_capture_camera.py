3a4fcfef622d9439e5d50141da82e0c3e6e79384
Aditya Sreekumar <aditya.sreekumar@gmail.com>
2018-04-08 17:57:08 -0400

    Modify capture camera nodee

--- src/camera_support/scripts/node_capture_camera.py
+++ src/camera_support/scripts/node_capture_camera.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 '''
 '''
+import sys
 import rospy
-from std_msgs.msg import String
-from sensor_msgs.msg import Image, CameraInfo
-
 import cv2
-#import cv2.cv as cv
+
+from sensor_msgs.msg import Image, CameraInfo
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -14,38 +13,33 @@ from cv_bridge import CvBridge, CvBridgeError
 class ImageCapture:
 
     def __init__(self):
-        self.node_name = 'CameraInput'
 
+        self.node_name = 'ImageCapture'
         rospy.init_node(self.node_name, anonymous=True)
 
         # What we do during shutdown
-        # rospy.on_shutdown(self.cleanup)
+        rospy.on_shutdown(self.cleanup)
 
-        # Create the OpenCV display window for the RGB image
-        self.cv_window_name = self.node_name
-        cv2.namedWindow(self.cv_window_name, cv2.WINDOW_NORMAL)
-        cv2.moveWindow(self.cv_window_name, 25, 75)
         # Create the cv_bridge object
         self.bridge = CvBridge()
 
+        # Subscribe to the
         self.image_sub = rospy.Subscriber("/cv_camera/image_raw", Image, self.image_capture_callback)
 
     def image_capture_callback(self, opencv_image):
         # Use cv_bridge() to convert the ROS image to OpenCV format
         try:
-            frame = self.bridge.imgmsg_to_cv2(opencv_image, "bgr8")
-        except CvBridgeError, e:
-            print e
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
 
         # Convert the image to a Numpy array since most cv2 functions
         # require Numpy arrays.
-        frame = np.array(frame, dtype=np.uint8)
-
-        # Process the frame using the process_image() function
-        display_image = self.process_image(frame)
+        # @todo - Create a topic to publish this data for opencv processing
+        # np_data = np.array(frame, dtype=np.uint8)
 
         # Display the image.
-        cv2.imshow(self.node_name, display_image)
+        cv2.imshow(self.node_name, cv_image)
 
     def process_image(self, frame):
         # Convert to greyscale
@@ -59,9 +53,15 @@ class ImageCapture:
 
         return edges
 
+    def cleanup(self):
+        cv2.destroyAllWindows()
+
 if __name__ == '__main__':
 
     # Init the raspirobot with the right voltages
     ImageCapture()
 
-    rospy.spin()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
--- src/camera_support/scripts/node_test_cvbridge.py
+++ src/camera_support/scripts/node_test_cvbridge.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
-from __future__ import print_function
-
-import roslib
-#roslib.load_manifest('my_package')
 import sys
 import rospy
 import cv2
-from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -20,13 +15,10 @@ class image_converter:
 
   def callback(self,data):
     try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      #cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      cv_image = data
     except CvBridgeError as e:
       print(e)
-
-    (rows,cols,channels) = cv_image.shape
-    if cols > 60 and rows > 60 :
-      cv2.circle(cv_image, (50,50), 10, 255)
 
     cv2.imshow("Image window", cv_image)
     cv2.waitKey(3)
